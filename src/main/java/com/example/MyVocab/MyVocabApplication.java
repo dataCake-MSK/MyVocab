90be1b97395ddcfa936724b56639fe6284a03cc9
Minsung Kang <minsungkang2012@gmail.com>
2025-04-05 20:10:03 +0900

    First Commit

---
+++ src/main/java/com/example/MyVocab/MyVocabApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyVocab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVocabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyVocabApplication.class, args);
+	}
+
+}
